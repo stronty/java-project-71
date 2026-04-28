a371a751761aec07c745eb6e5f258fb1aab3c27f
stronty <strontium8762aem@gmail.com>
2026-04-28 15:14:11 +0300

    initiate the project

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    application
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.2.3.7755"
+
+}
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "stronty_java-project-61")
+        property("sonar.organization", "stronty")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
